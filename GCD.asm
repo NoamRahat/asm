c7d9a106fe671a8ea2bebaba0964921426629775
NoamRahat <96725773+NoamRahat@users.noreply.github.com>
2022-05-16 08:33:29 +0300

    Update GCD.asm

--- GCD.asm
+++ GCD.asm
@@ -4,21 +4,20 @@
 .stack 100h
 .data
 	result dw ?
-	input_arr dw 3024, 1234, 1244, 44, 12414
-	inputLen EQU 5
-	asciiVal db "0123456789ABCDEF" ;Auxiliary array for screen printing
+	input_arr dw 1234, 3096, 12440, 2048
+	inputLen EQU 4
 .code
 
-;input: AX, BX
-;output: swap(AX, BX)
+;Input: AX, BX
+;Output: swap(AX, BX)
 swap proc
 	xor ax, bx
 	xor bx, ax
 	xor ax, bx
 swap endp
 
-;input: AX, BX
-;output: ds:result = GCD(AX, BX)
+;Input: AX, BX
+;Output: ds:result = GCD(AX, BX)
 recGCD proc uses dx
 	;stopping condition: if BX = 0 then return AX.
 	cmp bx, 0
@@ -36,12 +35,12 @@ stop:
 	ret
 recGCD endp
 
-;input: an array of words (DW) named arr_input, SI = array index, CX = array length 
-;output: AX = GCD(input_arr)
+;Input: an array of words (DW) named arr_input, SI = array index, CX = array length 
+;Output: AX = GCD(input_arr)
 arrGCD proc uses bx
 	mov ax, input_arr[si]
 	mov bx, result[0]
-	add si, 2
+	add si, 2	;promote array index
 	call recGCD
 	;stopping condition: if CX = 0 then finish.
 	dec cx
@@ -53,50 +52,47 @@ finish:
 	ret
 arrGCD endp
 
-;input: AX
-;otput: print AX to screen
-printAX proc uses bx cx es
-	mov cx, 4	;will be used in the isolation procedure
-	;dealing with AH value, without changing AX register
-	mov bl, ah 
-	shr bl, cl			;isolating the 4 msb of AX by shifting 4 bits
-	mov bh, 0
-	mov bl, asciiVal[bx]		;offset to the hexa digit
-	mov bh, 0Fh			;writing to screen memory
-	mov es:[280h+96h], bx ;printing to the sreen
-
-	mov bl, ah 			;duplicate ax
-	shl bl, cl			;isolating the 4 next msb of AX by shifting 4 bits
-	shr bl, cl
-	mov bh, 0
-	mov bl, asciiVal[bx]		;offset to the hexa digit
-	mov bh, 0Fh			;writing to screen memory
-	mov es:[280h+98h], bx ;printing to the sreen
-
-	;AL - the same process
-	;dealing with AL value, without changing AX register
-	mov bl, al 			;duplicate ax
-	shr bl, cl			;isolating the next 4 msb of AX by shifting 4 bits
-	mov bh, 0
-	mov bl, asciiVal[bx]		;offset to the hexa digit
-	mov bh, 0Fh			;writing to screen memory
-	mov es:[280h+9Ah], bx ;printing to the sreen
-
-	mov bl, al 	;duplicate ax
-	shl bl, cl	;isolating the next 4 LSB of AX by shifting 4 bits
-	shr bl, cl
-	mov bh, 0
-	mov bl, asciiVal[bx]		;offset to the hexa digit
-	mov bh, 0Fh			;writing to screen memory					
-	mov es:[280h+9Ch], bx ;printing to the sreen
-	ret
-printAX endp
+;Input: AX
+;Otput: print AX (decimal) to screen
+print proc uses cx dx   
+    ;initialize count
+    mov cx,0
+    mov dx,0
+    label1:
+        ;if ax is zero
+        cmp ax, 0
+        je print1     
+        ;initialize bx to 10
+        mov bx, 10       
+        ;extract the last digit
+        div bx                 
+        ;push it in the stack
+        push dx             
+        ;increment the count
+        inc cx             
+        ;set dx to 0
+        xor dx, dx
+        jmp label1
+    print1:
+        ;check if count is greater than zero
+        cmp cx,0
+        je exit
+        ;pop the top of stack
+        pop dx
+        ;add 48 so that it represents the ASCII value of digits
+        add dx,48
+        ;interrupt to print a character
+        mov ah,02h
+        int 21h
+        ;decrease the count
+        dec cx
+        jmp print1
+exit:
+ret
+print endp
 
 main:
-	;setting data segment
-	mov ax, @data
-	mov ds, ax
-
+	.startup
 	;setting extra segment to screen memory
 	mov ax, 0B800h
 	mov es, ax
@@ -105,7 +101,7 @@ main:
 	mov cx, inputLen ;init array length.
 	call arrGCD
 	
-	call printAX
+	call print
 	
 	.exit
 end main
