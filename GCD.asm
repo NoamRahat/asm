fdf4c428329bf0a99a9a615b54c51f1fc5577668
NoamRahat <96725773+NoamRahat@users.noreply.github.com>
2022-05-16 10:48:28 +0300

    Update GCD.asm

--- GCD.asm
+++ GCD.asm
@@ -54,48 +54,21 @@ arrGCD endp
 
 ;Input: AX
 ;Otput: print AX (decimal) to screen
-print proc uses cx dx   
-    ;initialize count
-    mov cx,0
-    mov dx,0
-    label1:
-        ;if ax is zero
-        cmp ax, 0
-        je print1     
-        ;initialize bx to 10
-        mov bx, 10       
-        ;extract the last digit
-        div bx                 
-        ;push it in the stack
-        push dx             
-        ;increment the count
-        inc cx             
-        ;set dx to 0
-        xor dx, dx
-        jmp label1
-    print1:
-        ;check if count is greater than zero
-        cmp cx,0
-        je exit
-        ;pop the top of stack
-        pop dx
-        ;add 48 so that it represents the ASCII value of digits
-        add dx,48
-        ;interrupt to print a character
-        mov ah,02h
-        int 21h
-        ;decrease the count
-        dec cx
-        jmp print1
-exit:
+print proc uses dx
+    mov dx, ax
+    ;add 48 so that it represents the ASCII value of digits
+    add dx,48
+    ;interrupt to print a character
+    mov ah,02h
+    int 21h
 ret
 print endp
 
 main:
 	.startup
 	;setting extra segment to screen memory
-	mov ax, 0B800h
-	mov es, ax
+	;mov ax, 0B800h
+	;mov es, ax
 	
 	mov si, 0 ;init array index.
 	mov cx, inputLen ;init array length.
