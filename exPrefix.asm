2589c08dffdd97d992dc6392fce5004da28d4003
NoamRahat <96725773+NoamRahat@users.noreply.github.com>
2022-05-17 19:39:41 +0300

    Update exPrefix.asm

--- exPrefix.asm
+++ exPrefix.asm
@@ -2,9 +2,8 @@
 .stack 200h
 .data
 Number dw 2020h
-decimalDigArr db (5) dup (?)
+decimalDigStack db (5) dup (?)
 .code
-
 ;Input: top stack element (n)
 ;Output: Prints the following lines:
 ;In the first line the whole number will appear in its decimal value.
@@ -12,28 +11,25 @@ decimalDigArr db (5) dup (?)
 ;In the next line - the number in its decimal value without the tens and ones digits, and so on.
 ;Until the last line is printed with the most significant digit of the number in its decimal value.
 numPrefix proc uses dx bp si bx
-	;Get AX = Number
+	;Get AX = Number (parameter)
 	mov bp, sp
 	mov ax, [bp + 10]
-	
-	;stopping condition: ax = 0
+	;Stopping condition: AX = 0
 	cmp ax, 0
 	je finish
-	
-	;initialize count
+	;Initialize counters
 	mov si, 0 ;countes number of digits
 	mov dx, 0
-	
+	;Push digits into decimalDigStack
 	push ax
 	call lastDigIntoStack
 	pop ax
-	
+	;Print AX in decimal presentation
 	call print
 	call newline
-	
 	;Next Call
-	mov bx, 10 	;initialize bx to 10
-	xor dx, dx	;set dx to 0
+	mov bx, 10 	;initialize BX to 10
+	xor dx, dx	;set DX to 0
 	div bx		;AX = AX / 10
 	push ax		;Pass new value by stack
 	call numPrefix
@@ -42,23 +38,23 @@ finish:
 numPrefix endp
 
 ;Input: AX
-;Output: Array of ax value in decimal seperated by digits
+;Output: Array of AX value in decimal seperated by digits
 lastDigIntoStack proc
-	;stopping condition: if AX is zero
+	;Stopping condition: if AX is zero
 	cmp ax, 0
 	je DONE
-	;initialize bx to 10
+	;Initialize bx to 10
 	mov bx, 10       
-	;extract the last digit DX = remainder (modulus)
+	;Extract the last digit DX = remainder (modulus)
 	div bx                 	 
-	;push it in the memorry
-	mov decimalDigArr[si], dl            	 
+	;Push it in the memorry
+	mov decimalDigStack[si], dl            	 
 	;increment the count of digits number
 	inc si             	 
-	;set dx to 0
+	;Set dx to 0
 	xor dx, dx
 	jmp NextCall
-DONE:	;when stoping condition exists
+DONE:	;When stoping condition exists
 	ret
 NextCall:
 	call lastDigIntoStack
@@ -67,47 +63,44 @@ lastDigIntoStack endp
 ;Input: decimalDigArr
 ;Output: print decimalDigArr to screen
 print proc uses ax dx
-	;stopping condition: check if number of digits is greater than zero
+	;Stopping condition: check if number of digits is greater than zero
 	cmp si, 0
 	je exit
-	
-	;pop the top of stack memmory
-	mov dl, decimalDigArr[si-1]
-	;add 48 so that it represents the ASCII value of digits
+	;Pop the top of stack memmory
+	mov dl, decimalDigStack[si-1]
+	;Add 48 so that it represents the ASCII value of digits
 	add dl, 48
-	
-	;interrupt to print a character
+	;Interrupt to print a character
 	mov ah, 02h
 	int 21h
-	;decrease the count
+	;Decrease the count
 	dec si
 	
-	call print ;repeat
+	call print ;Repeat
 exit:
 	ret
 print endp
 
-;interupt printing dx value
+;Interupt printing DX value: 10d in ASCII code - LF
 newline proc uses ax dx
-	mov dl, 10	;line feeding
+	mov dl, 10	;Line feeding
 	mov ah, 02h
 	int 21h
-	mov dl, 13	;carriage return
+	mov dl, 13	;Carriage return
 	mov ah, 02h
 	int 21h
 	ret
 newline endp
 
-
+;Main Driver
 main:
 	.startup
-	;load the value stored in variable Nember into stack
+	;Load the value stored in variable Nember into stack
 	push ax
 		mov ax, Number
 		push ax
 			call numPrefix
 		pop ax
 	pop ax
-
 	.exit
 end main
