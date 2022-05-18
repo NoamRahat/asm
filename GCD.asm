7b1584c40c3597e8e12981962fead3c8b3331c3e
NoamRahat <96725773+NoamRahat@users.noreply.github.com>
2022-05-18 11:12:29 +0300

    Update GCD.asm

--- GCD.asm
+++ GCD.asm
@@ -1,11 +1,10 @@
 ;This program clculates the GCD of an input array rucursevly
-
 .model small
 .stack 100h
 .data
 	result dw ?
-	input_arr dw 1234, 3096, 12440, 2048
-	inputLen EQU 4
+	input_arr dw (2000) dup (2000, 1234, 3096, 12440, 2048)
+	inputLen EQU 2000
 .code
 
 ;Input: AX, BX
@@ -54,24 +53,29 @@ arrGCD endp
 
 ;Input: AX
 ;Otput: print AX (decimal) to screen
-print proc uses dx
+print proc uses dx ax
     mov dx, ax
     ;add 48 so that it represents the ASCII value of digits
     add dx, 48
     ;interrupt to print a character
     mov ah, 02h
     int 21h
+	;Interupt printing DX value: 10d in ASCII code - LF
+	mov dl, 10	;Line feeding
+	mov ah, 02h
+	int 21h
+	mov dl, 13	;Carriage return
+	mov ah, 02h
+	int 21h
 ret
 print endp
 
 main:
 	.startup
-	
 	mov si, 0 ;init array index.
 	mov cx, inputLen ;init array length.
 	call arrGCD
 	
 	call print
-	
 	.exit
 end main
